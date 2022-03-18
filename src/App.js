cc2ea2add062a5e6bfb4070c5c636d6dab18ead6
marsianca <xmarsiancax@mail.ru>
2022-03-18 10:46:28 +0300

    добавление запроса на получение погоды за 7 дней

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Footer from "./components/footer/footer";
 import s from "./App.module.css"
 import NavBarContainer from "./components/navBar/navBarContainer";
 import ContentContainer from "./components/content/contentContainer";
+import {getDailyWeather} from "./services/weather-service";
 
 
 
 function App() {
+    console.log(getDailyWeather("Moscow", 4));
     return (
     <div className={s.wr}>
         <div className={s.h}><HeaderContainer/></div>
--- src/services/weather-service.js
+++ src/services/weather-service.js
@@ -4,6 +4,11 @@ export const getCurrentWeather = (cityName) => {
     return axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=9d64cb7219261444377ecf9c36ed62d9`);
 }
 
+export const getDailyWeather = (coord) => {
+    return axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${coord.lat}&lon=${coord.lon}
+    &exclude=current,minutely,hourly&appid=9d64cb7219261444377ecf9c36ed62d9`);
+}
+
 export const getWeather = (cityName) => {
     return new Promise(((resolve, reject) => {
         fetch(
